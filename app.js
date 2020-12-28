75e7524d5c0e60d535cce0ae1b47eb96121eb105
LivingWill <william.hinson@outlook.com>
2020-12-28 06:27:27 -0600

    basic html and opperators created

---
+++ app.js
@@ -0,0 +1,14 @@
+const screen = document.querySelector('#screen');
+
+
+
+
+
+
+const add = (x, y) =>  x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+const operate = (opp, x, y) => opp(x, y);
+
+let displayValue = 0;
