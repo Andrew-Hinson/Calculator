91e1bdf024a7134db8c2f1c7b6c869b1af37067d
LivingWill <william.hinson@outlook.com>
2021-01-01 21:28:25 -0600

    additional buttons added, delete functionality added

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const numbers =  document.querySelectorAll('.numbers');
 const opperator = document.querySelectorAll('.opp');
 const cleared = document.querySelector('#clearbtn');
 const equals = document.querySelector('#equals');
-
+const deleteBtn = document.querySelector('#deletebtn');
+let hasPressed = false;
 let currentOpp;
 let opperand;
 let numToBeEvaled = ''; //string that is eventually parsed into array and spit back out
@@ -17,16 +18,43 @@ const evalObj = {
 }
 
 let updateNum = (element) => {
-      //add evaluation to this function (ongoing evaluation)    
+      
     screen.innerText += parseInt(element.target.value);
     numToBeEvaled += element.target.value
     console.log(numToBeEvaled)
 }
 let updateOpp = (element) => {
+    if(hasPressed == true){
+        myEval()
+    }
+    hasPressed = true
     screen.innerText += element.target.value
     currentOpp = element.target.value.charCodeAt(0)
     numToBeEvaled += element.target.value
+    
 }
+
+let myEval = () => {
+    let numArr = numToBeEvaled.split(/[^\w\s]|_/g)
+    if (currentOpp == 43){
+        opperand = evalObj.add
+    } else if (currentOpp == 45){
+        opperand = evalObj.sub
+    } else if (currentOpp == 42){
+        opperand = evalObj.mult
+    } else if (currentOpp == 47){
+        opperand = evalObj.div
+    }
+    let totalArr = numArr
+        .map(i => parseInt(i))
+        .reduce(opperand)
+        .toString()
+    total = totalArr;
+    screen.innerText = totalArr;
+    numToBeEvaled = total; 
+    hasPressed = true;
+}
+
 
 equals.addEventListener('click', () => {
     let numArr = numToBeEvaled.split(/[^\w\s]|_/g)
@@ -41,19 +69,29 @@ equals.addEventListener('click', () => {
     }
     let totalArr = numArr
         .map(i => parseInt(i))
-        .reduce(opperand);
-
+        .reduce(opperand)
+        .toString()
     total = totalArr;
     screen.innerText = totalArr;
-    numToBeEvaled = total   //needs to be 
+    numToBeEvaled = total 
 })
 
 cleared.addEventListener('click', () => {
     screen.innerText = '';
     numToBeEvaled = ''
     total = 0;
+    currentOpp = 0;
+    hasPressed = false;
 });
-//
+deleteBtn.addEventListener('click', () => {
+    let newStr = numToBeEvaled.slice(0, numToBeEvaled.length -1)
+    numToBeEvaled = newStr
+    total = newStr
+    screen.innerText = newStr
+    
+})
+
+
 
 numbers.forEach((number) => number.addEventListener('click', updateNum));
 
