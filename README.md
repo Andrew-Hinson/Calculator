bd043215279ac72c12e31b8e2fc077fad484f4d7
LivingWill <william.hinson@outlook.com>
2021-01-05 21:29:19 -0600

    working on simplifying code, added todos to README

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 Odin Project Calculator 
 Guidelines for this project can be found at: https://www.theodinproject.com/courses/foundations/lessons/calculator
 
-To Do: support for negative numbers, host app.
+To Do: 
+1. support for negative numbers
+2. Decimal is not working after refactor, allows for more than one per number
+3. Styling; responsive needs improvement, jumbled mess in mobile format, stretched too much in wide
+4. Every now and then pressing enter returns blank screen.
+5. Need to evaluate numbers and maybe give the power of so that the screen space is not taken up so much.
--- app.js
+++ app.js
@@ -11,7 +11,6 @@ let currentOpp;  //+ - * / assigned unicode val
 let opperand;
 let numToBeEvaled = ''; //string that is eventually parsed into array and spit back out
 let total = 0;
-let decPress = false;
 let keyVal;
 //currently it just appends the value to the displayVal variable and shows that in the text
 const evalObj = {
@@ -91,36 +90,36 @@ deleteBtn.addEventListener('click', () => {
     deleteOpp()
 })
 
-decimal.addEventListener('click', () => {
-    decimal.disabled = true;
-})
+// decimal.addEventListener('click', () => {
+//     decimal.disabled = true;
+// })
 
-body.addEventListener('click', () => {
+
+const activeFontSizeChanger = () => {
     if(numToBeEvaled.length >= 9){
         screen.style.fontSize = '3em';
     } else {
         screen.style.fontSize = '5em';
     }
-})
-body.addEventListener('keydown', () => {
-    if(numToBeEvaled.length >= 9){
-        screen.style.fontSize = '3em';
-    } else {
-        screen.style.fontSize = '5em';
-    }
-})
+}
 
+body.addEventListener('click', activeFontSizeChanger);
+body.addEventListener('keydown', activeFontSizeChanger);
+//rewrite to - on enter or opperand, parse keyVal to integer
 document.addEventListener('keydown', (e) => {
     if(e.key >= 0 && e.key <= 9){
         if(numToBeEvaled.length >= 19){
             numbers.attributes.disabled = true;
         }
-    keyVal = e.key
-    screen.innerText += e.key
-    numToBeEvaled += e.key
+        keyVal = e.key              //needs to be inside first conditional, only want values
+        screen.innerText += e.key
+        numToBeEvaled += e.key
     }
     if(e.key == 'Backspace'){
         deleteOpp()
+    }
+    if(e.key == '.'){
+        e.key.disabled = true;
     }
     if(e.key == 'Enter'){
         evaluate()
@@ -131,6 +130,7 @@ document.addEventListener('keydown', (e) => {
         numToBeEvaled += e.key
         currentOpp = oppPress
     }
+    
 });
 
 numbers.forEach((number) => number.addEventListener('click', (numPress) =>{
